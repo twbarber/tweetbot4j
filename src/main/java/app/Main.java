7e38617776f5ae9cb185c99f4ec25efe66af41ef
twbarber <tylerwbarber@gmail.com>
2016-01-17 15:28:30 -0600

    Adding sl4j, twitter4j, and junit

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,12 +1,14 @@
 package app;
 
+import twitter.Tweetbot;
+
 /**
  * Main class for running Jar.
  */
 public class Main {
 
   public static void main(String[] args) {
-
+    Tweetbot tweetbot = new Tweetbot();
   }
 
 }
